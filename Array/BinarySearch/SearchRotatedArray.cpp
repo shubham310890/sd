618fdffefce27ef9f4c1ddc68c45482c9f4e3914
Shubham Shrivastav <shubham.iiitm@gmail.com>
2021-06-07 22:07:47 +0530

    Create SearchRotatedArray.cpp

---
+++ Array/BinarySearch/SearchRotatedArray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
+        while(low <= high) {
+            if(low == high) {
+                return nums[low] == target ? low : -1;
+            }
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] < nums[high]) {
+                if(target > nums[mid] && target <= nums[high]) {
+                    low = mid+1;
+                } else {
+                    high = mid-1;
+                }
+            } else {
+                if(target >= nums[low] && target < nums[mid]) {
+                    high = mid-1;
+                } else {
+                    low = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+};
